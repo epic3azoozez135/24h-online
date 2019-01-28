0cf928132ab4eed4b1fa651b4b7a298eb009e2bb
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2019-01-28 17:06:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,1 @@
-const Discord = require("discord.js")
-const client = new Discord.Client();
-var prefix = "A"
 
-
-
-const gamestats = [`I`,`Love`,`You!`,``]
-var index = 0
-var timer = 3 // الوقت بالثواني لتغير الستريمنق
-client.on("ready", ()=> {
-        setInterval(function(){
-        client.user.setGame(`${gamestats[index]}`,'https://www.twitch.tv/ACMBOT') 
-        index++
-            if( index >= gamestats.length) index = 0 ;
-        }, timer*1000);
-
-client.login(process.env.BOT_TOKEN);
